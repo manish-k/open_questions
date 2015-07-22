e84d79e126ab9164bb8577f25baecb7aa3c3dc64
Gaurav Chakravorty <gchak@circulumvite.com>
2015-07-22 10:25:45 -0400

    checking solution

--- algoques/check_soln.sh
+++ algoques/check_soln.sh
@@ -16,6 +16,11 @@ cd $our_repo/algoques;
 git checkout sample;
 g++ -O3 process_data.cpp -o $HOME/our_soln;
 
+if [ ! -e $HOME/our_soln ] ; then
+    echo "Exec $HOME/our_soln not built. Aborting"
+    exit;
+fi
+
 diffcount=`diff -w $our_repo/algoques/process_data.cpp $soln_repo/algoques/process_data.cpp | wc -l`;
 if [ $diffcount -gt 0 ] ; then 
   echo "$our_repo/algoques/process_data.cpp $soln_repo/algoques/process_data.cpp differs.";
@@ -25,6 +30,10 @@ fi
 cd $soln_repo/algoques;
 g++ -O3 process_data.cpp -o $HOME/soln;
 
+if [ ! -e $HOME/soln ] ; then
+    echo "Exec $HOME/soln not built. Aborting"
+    exit;
+fi
 
 cmd="$HOME/soln $our_repo/algoques/sample_random_forest.txt $inputfile $HOME/output 30 2>/dev/null";
 echo $cmd;
@@ -33,6 +42,14 @@ $cmd 2>/dev/null;
 cmd="$HOME/our_soln $our_repo/algoques/sample_random_forest.txt $inputfile $HOME/our_output 30 2>/dev/null";
 echo $cmd;
 $cmd 2>/dev/null;
+
+if [ ! -e $HOME/output ] ; then
+    echo "$HOME/output not generated"; exit
+fi
+
+if [ ! -e $HOME/our_output ] ; then
+    echo "$HOME/our_output not generated"; exit
+fi
 
 #Removing outputs untill all indicators are updated atleast once
 sed -i 1,26d $HOME/our_output;
